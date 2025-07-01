d71b667d3ecc9a9389fe45dde2e01d94bb4ba3a9
amonra <przemyslaw.mlak@student.uj.edu.pl>
2025-07-01 10:55:38 +0200

    seventh commit

--- api/getWeeklyForecast.ts
+++ api/getWeeklyForecast.ts
@@ -1,5 +1,3 @@
 import { getWeather } from "../controllers/weatherController";
 
-export default async function handler(req: Request): Promise<Response> {
-    return await getWeather(req);
-}
+export default getWeather;
