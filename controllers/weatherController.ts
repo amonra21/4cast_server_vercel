879b29daf70938581902e7037e59f1f79e84931f
amonra <przemyslaw.mlak@student.uj.edu.pl>
2025-07-01 11:11:08 +0200

    ninth commit

--- controllers/weatherController.ts
+++ controllers/weatherController.ts
@@ -1,20 +1,30 @@
 import { fetchWeatherData } from "../services/fetchWeatherData";
 
-export const getWeather = async (req, res) => {
+export const getWeather = async (req:any, res:any) => {
     try {
         const url = new URL(req.url, `https://${req.headers.host}`);
         const lat = url.searchParams.get("lat");
         const lon = url.searchParams.get("lon");
 
         if (!lat || !lon) {
-            return res.status(400).json({ error: "Brakuje współrzędnych (lat i lon)" });
+            return res
+                .setHeader("Access-Control-Allow-Origin", "*")
+                .status(400)
+                .json({ error: "Brakuje współrzędnych (lat i lon)" });
         }
 
         const data = await fetchWeatherData(parseFloat(lat), parseFloat(lon));
-        return res.status(200).json(data);
+
+        return res
+            .setHeader("Access-Control-Allow-Origin", "*")
+            .status(200)
+            .json(data);
 
     } catch (error) {
         console.error("Błąd:", error);
-        return res.status(500).json({ error: "Błąd podczas pobierania danych pogodowych" });
+        return res
+            .setHeader("Access-Control-Allow-Origin", "*")
+            .status(500)
+            .json({ error: "Błąd podczas pobierania danych pogodowych" });
     }
 };
